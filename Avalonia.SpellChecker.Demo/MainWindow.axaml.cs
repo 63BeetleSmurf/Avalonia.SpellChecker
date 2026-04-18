7801b8b4d982e64e9d57d3265e1e2cabda7ca44b
63BeetleSmurf <scott@murphys.place>
2026-04-18 22:26:14 +0100

    Update demo to show enabled options.

--- Avalonia.SpellChecker.Demo/MainWindow.axaml.cs
+++ Avalonia.SpellChecker.Demo/MainWindow.axaml.cs
@@ -10,6 +10,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        cbGlobalSpellCheckingEnable.IsCheckedChanged += cbGlobalSpellCheckingEnable_IsCheckedChanged;
+        cbUserNameSpellCheckingEnable.IsCheckedChanged += cbUserNameSpellCheckingEnable_IsCheckedChanged;
+        cbNotesSpellCheckingEnable.IsCheckedChanged += cbNotesSpellCheckingEnable_IsCheckedChanged;
+
         tbNotes.UpdateLayout();
         //    TbNotes.Text
 
@@ -25,6 +30,50 @@ public partial class MainWindow : Window
     public void CustomAction_Click(object? sender, RoutedEventArgs args)
     {
 
+    }
+
+    private void cbGlobalSpellCheckingEnable_IsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not CheckBox checkBox)
+        {
+            return;
+        }
+
+        textBoxSpellChecker.IsEnabled = (checkBox.IsChecked == true);
+    }
+
+    private void cbUserNameSpellCheckingEnable_IsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not CheckBox checkBox)
+        {
+            return;
+        }
+
+        if (checkBox.IsChecked == true)
+        {
+            textBoxSpellChecker.Enable(tbUserName);
+        }
+        else
+        {
+            textBoxSpellChecker.Disable(tbUserName);
+        }
+    }
+
+    private void cbNotesSpellCheckingEnable_IsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not CheckBox checkBox)
+        {
+            return;
+        }
+
+        if (checkBox.IsChecked == true)
+        {
+            textBoxSpellChecker.Enable(tbNotes);
+        }
+        else
+        {
+            textBoxSpellChecker.Disable(tbNotes);
+        }
     }
 
 }
